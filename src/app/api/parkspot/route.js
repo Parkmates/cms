adc7e7a17fd4de8abe3510d3bf19cee582f88552
febriantopermana <febriantopermana16@gmail.com>
2024-09-03 18:20:15 +0700

    feature crate parking spot API POST /parkspot

--- src/app/api/parkspot/route.js
+++ src/app/api/parkspot/route.js
@@ -1,15 +1,40 @@
-const ParkingSpotModels = require("@/db/models/parkingSpot")
+const ParkingSpotModels = require("@/db/models/parkingSpot");
 
 async function GET() {
-    try {
-        const result = await ParkingSpotModels.getAll()
-        return Response.json(result)
-    } catch (error) {
-        console.log(error)
-        Response.json(error)
-    }
+  try {
+    const result = await ParkingSpotModels.getAll();
+    return Response.json(result);
+  } catch (error) {
+    console.log(error);
+    Response.json(error);
+  }
+}
+
+async function POST(req) {
+  try {
+    const { name, address, imgUrl, motorSpot, carSpot } = await req.json();
+    if (!name) throw { name: "RequiredName" };
+    if (!address) throw { name: "RequiredAddress" };
+    if (!imgUrl) throw { name: "RequiredImgUrl" };
+    if (!motorSpot) throw { name: "RequiredMotorSpot" };
+    if (!carSpot) throw { name: "RequiredCarSpot" };
+
+    const result = await ParkingSpotModels.createParkingSpot({
+      name,
+      address,
+      imgUrl,
+      motorSpot,
+      carSpot,
+    });
+
+    return Response.json({ msg: result });
+  } catch (error) {
+    console.log(error);
+    return Response.json(error);
+  }
 }
 
 module.exports = {
-    GET
-}
+  GET,
+  POST,
+};
