3f006feaffa5f4c51b284e16447fb4dd10feebe4
febriantopermana <febriantopermana16@gmail.com>
2024-09-03 11:47:47 +0700

    add db, config for mongodb, models for userModels

---
+++ src/db/config/mongodb.js
@@ -0,0 +1,16 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://febriantopermana16:fp160201@febh8.p4n2u.mongodb.net/?retryWrites=true&w=majority&appName=FebH8";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const database = client.db("FinalProject")
+
+module.exports = database
