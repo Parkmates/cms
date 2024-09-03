1d575a92d2f5f6f398dbc90319db58c800a37283
febriantopermana <febriantopermana16@gmail.com>
2024-09-03 22:32:39 +0700

    merge fix

--- src/app/api/parkspot/route.js
+++ src/app/api/parkspot/route.js
@@ -6,17 +6,13 @@ async function GET() {
     return Response.json(result);
   } catch (error) {
     console.log(error);
-<<<<<<< Updated upstream
     return Response.json(error);
-=======
     Response.json(error);
->>>>>>> Stashed changes
   }
 }
 
 async function POST(req) {
   try {
-<<<<<<< Updated upstream
     const { name, address, imgUrl, motorSpot, carSpot, motorFee, carFee } = await req.json();
     if (!name) throw { name: "RequiredName" };
     if (!address) throw { name: "RequiredAddress" };
@@ -25,37 +21,21 @@ async function POST(req) {
     if (!carSpot) throw { name: "RequiredCarSpot" };
     if (!motorFee) throw { name: "RequiredMotorFee" };
     if (!carFee) throw { name: "RequiredCarFee" };
-=======
-    const { name, address, imgUrl, motorSpot, carSpot } = req.json();
-    if (!name) throw { name: "RequiredName" };
-    if (!address) throw { name: "RequiredName" };
-    if (!imgUrl) throw { name: "RequiredName" };
-    if (!motorSpot) throw { name: "RequiredName" };
-    if (!carSpot) throw { name: "RequiredName" };
->>>>>>> Stashed changes
 
     const result = await ParkingSpotModels.createParkingSpot({
       name,
       address,
       imgUrl,
       motorSpot,
-<<<<<<< Updated upstream
       carSpot,
       motorFee,
       carFee
-=======
-      CarSpot,
->>>>>>> Stashed changes
     });
 
     return Response.json({ msg: result });
   } catch (error) {
     console.log(error);
-<<<<<<< Updated upstream
     return Response.json(error);
-=======
-    Response.json(error);
->>>>>>> Stashed changes
   }
 }
 
