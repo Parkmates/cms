ecb8d683dfd835ac2494a8346da09d572db4bb41
febriantopermana <febriantopermana16@gmail.com>
2024-09-03 20:43:26 +0700

    add feature getById Transaction

--- src/db/models/transaction.js
+++ src/db/models/transaction.js
@@ -10,11 +10,20 @@ class TransactionModels {
     return transactions;
   }
 
+  static async getById(id) {
+    const transaction = await database
+      .collection("transactions")
+      .findOne({ _id: new ObjectId(String(id)) });
+    if (!transaction) throw { name: "TransactionNotFound" };
+
+    return transaction;
+  }
+
   static async createTransaction({ spotId }) {
     const spotValidate = await database
       .collection("transactions")
       .findOne({ spotId: new ObjectId(String(spotId)) });
-    if(spotValidate) throw { name: "AlreadyBookSpot"}
+    if (spotValidate) throw { name: "AlreadyBookSpot" };
 
     const result = await database.collection("transactions").insertOne({
       userId: new ObjectId("66d6d3d0cf201705437e09cc"),
