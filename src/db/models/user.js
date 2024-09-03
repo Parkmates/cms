11896d4692255bb1c7ff212eaf84d27f8c1f9a2a
febriantopermana <febriantopermana16@gmail.com>
2024-09-03 15:41:02 +0700

    add feature api login

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,5 +1,20 @@
-class UserModels {
+const { comparePass } = require("@/helpers/bcrypt")
+const database = require("../config/mongodb")
+const { signToken } = require("@/helpers/jwt")
 
+class UserModels {
+    static async login({email, password}) {
+        const user = await database.collection("users").findOne({
+            email
+        })
+        if(!user) throw {name: "UserNotFound"}
+
+        const checkPass = comparePass(password, user.password)
+        if(!checkPass) throw {name: "WrongPass"}
+
+        const token = signToken({id: String(user._id), role: user.role})
+        return token
+    }
 }
 
 module.exports = UserModels
