5029cb893b07dea4d1dea943e941b267ad937710
zahara120 <zaharaa120@gmail.com>
2024-09-06 09:34:13 +0700

    feat: add vendor

--- src/app/api/users/addVendor/route.js
+++ src/app/api/users/addVendor/route.js
@@ -2,36 +2,40 @@ const UserModels = require("@/db/models/user");
 const { z } = require("zod");
 
 async function POST(req) {
-    try {
-        const userRole = req.headers.get("x-role")
-        const { name, username, email, password } = await req.json();
-        // if (!name) throw { name: "RequiredName" };
-        // if (!username) throw { name: "RequiredUsername" };
-        // if (!email) throw { name: "RequiredEmail" };
-        // if (!password) throw { name: "RequiredPassword" };
+  try {
+    const userRole = req.headers.get("x-role");
+    const { name, username, email, password } = await req.json();
+    const result = await UserModels.addVendor({
+      name,
+      username,
+      email,
+      password,
+      userRole,
+    });
+    return Response.json(result);
+  } catch (error) {
+    console.log(error);
+    let msgError = error.message || "Internal server error";
+    let status = 500;
 
-        const result = await UserModels.addVendor({ name, username, email, password, userRole })
-        return Response.json(result)
-    } catch (error) {
-        console.log(error)
-        let msgError = error.message || "Internal server error";
-        let status = 500;
-
-        if (error instanceof z.ZodError) {
-            msgError = error.errors[0].path[0] + " " + error.errors[0].message;
-            status = 400;
-        }
-        return Response.json(
-            {
-                msg: msgError,
-            },
-            {
-                status: status,
-            }
-        );
+    if (error instanceof z.ZodError) {
+      msgError = error.errors[0].path[0] + " " + error.errors[0].message;
+      status = 400;
     }
+    if (error.name === "Unauthorized") {
+      status = 401;
+    }
+    return Response.json(
+      {
+        msg: msgError,
+      },
+      {
+        status: status,
+      }
+    );
+  }
 }
 
 module.exports = {
-    POST
-}
+  POST,
+};
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -10,14 +10,28 @@ import {
   Tooltip,
   Image,
   useDisclosure,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Input,
 } from "@nextui-org/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
-// import { toast } from "react-toastify";
+import { toast } from "react-toastify";
 
-export default function ListMyRecipe() {
+export default function HomePage() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+  });
   const getData = async () => {
     const response = await fetch("/api/parkspot");
     const data = await response.json();
@@ -26,6 +40,32 @@ export default function ListMyRecipe() {
   useEffect(() => {
     getData();
   }, []);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(formData);
+      setIsLoading(true);
+      const response = await fetch("/api/users/addVendor", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) throw await response.json();
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.msg);
+    }
+  };
   return (
     <>
       <div className="p-4 md:px-12 md:py-7 md:mx-9 h-screen">
@@ -35,6 +75,7 @@ export default function ListMyRecipe() {
             <div className="flex items-center gap-2">
               {/* kalo role === admin */}
               <Button
+                onPress={onOpen}
                 className="bg-black text-white"
                 variant="flat"
                 startContent={
@@ -97,6 +138,73 @@ export default function ListMyRecipe() {
           </Table>
         </div>
       </div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="center">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Add vendor
+              </ModalHeader>
+              <ModalBody>
+                <Input
+                  isRequired
+                  label="Name"
+                  placeholder="Enter your name"
+                  type="text"
+                  variant="bordered"
+                  name="name"
+                  onChange={handleChange}
+                  value={formData.name}
+                />
+                <Input
+                  isRequired
+                  label="Username"
+                  placeholder="Enter your username"
+                  type="text"
+                  variant="bordered"
+                  name="username"
+                  onChange={handleChange}
+                  value={formData.username}
+                />
+                <Input
+                  isRequired
+                  label="Email"
+                  placeholder="Enter your email"
+                  type="email"
+                  variant="bordered"
+                  name="email"
+                  onChange={handleChange}
+                  value={formData.email}
+                />
+                <Input
+                  isRequired
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="text"
+                  variant="bordered"
+                  name="password"
+                  onChange={handleChange}
+                  value={formData.password}
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button
+                  type="submit"
+                  className="bg-black text-white"
+                  variant="flat"
+                  onClick={(e) => handleSubmit(e)}
+                  isLoading={isLoading}
+                >
+                  submit
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </>
   );
 }
