c6ac6c8624734787aee0a556d272bb8e74cc4842
zahara120 <zaharaa120@gmail.com>
2024-09-09 17:46:32 +0700

    feat: pwa done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+import withPWA from "next-pwa";
+
+export default withPWA({
+  dest: "public",
+  // Konfigurasi Next.js lainnya di sini
+});
